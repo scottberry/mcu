2e1069fedb4d7bde237db137e50be1603a5ff530
scottberry <scottdberry@gmail.com>
2019-10-21 21:20:58 +0000

    added files to get identities of border pixels

---
+++ mcu/borders.py
@@ -0,0 +1,154 @@
+import os
+import sys
+import logging
+import argparse
+import numpy as np
+import pandas as pd
+from tmclient import TmClient
+from mcu import pixel_profiles as pp
+
+logger = logging.getLogger()
+
+def get_borders(tm_credentials, experiment_name, metadata,
+    channel_names, object_type):
+
+    logger.debug('Establish connection to TM host')
+    tm = TmClient(
+        host=tm_credentials.host[0],
+        port=tm_credentials.port[0],
+        experiment_name=experiment_name,
+        username=tm_credentials.username[0],
+        password=tm_credentials.password[0]
+    )
+
+    size_factor = 0.5
+    n_pixels = int(size_factor * mean_object_size * metadata.shape[0])
+    n_channels = len(channel_names)
+
+    logger.debug('Initialise arrays')
+
+    # use an int32 array for mpp, as background subtraction will be performed later
+    border_mapobject_id_all = np.zeros((n_pixels,), dtype=np.uint32, order='C')
+    border_label_vector_all = np.zeros((n_pixels,), dtype=np.uint16, order='C')
+    border_y_coords_all = np.zeros((n_pixels,), dtype=np.uint16, order='C')
+    border_x_coords_all = np.zeros((n_pixels,), dtype=np.uint16, order='C')
+
+    # loop over the sites to generate the mpp
+    metadata = metadata.groupby(['plate_name','well_name','well_pos_y','well_pos_x'])
+    r = 0
+    for name, group in metadata:
+
+        if (r > n_pixels):
+            logger.error('Insufficient space in mpp array to store new pixels')
+            raise ValueError
+
+        logger.info('Extracting borders for {} objects in {} : {}, {}. Current overall pixel = {} (of total {})'.format(
+            group.shape[0],
+            group.iloc[0]['well_name'],
+            int(group.iloc[0]['well_pos_y']),
+            int(group.iloc[0]['well_pos_x']),
+            r, mpp_all.shape[0]))
+
+        label_vector, y_coords, x_coords = pp.get_borders_for_objects(
+            tm=tm,
+            object_type=object_type,
+            labels=group.label.tolist(),
+            plate_name=group.iloc[0]['plate_name'],
+            well_name=group.iloc[0]['well_name'],
+            well_pos_y=int(group.iloc[0]['well_pos_y']),
+            well_pos_x=int(group.iloc[0]['well_pos_x'])
+        )
+
+        p = len(label_vector)
+        logger.debug('Adding {} new border pixels'.format(p))
+
+        # get mapobject_ids for each label in this site
+        mapobject_id_dict = group[['label', 'mapobject_id']].set_index('label')['mapobject_id'].to_dict()
+
+        border_label_vector_all[r:r + p] = label_vector
+        border_mapobject_id_all[r:r + p] = np.vectorize(mapobject_id_dict.get)(label_vector)
+        border_y_coords_all[r:r + p] = y_coords
+        border_x_coords_all[r:r + p] = x_coords
+
+        # increment counter
+        r += p
+
+    # remove extra allocated space
+    logger.info('Extracted {} border pixels (of total {} allocated)'.format(r,mpp_all.shape[0]))
+    border_label_vector_all = border_label_vector_all[border_label_vector_all != 0]
+    border_mapobject_id_all = border_mapobject_id_all[border_mapobject_id_all != 0]
+    border_y_coords_all = border_y_coords_all[border_y_coords_all != 0]
+    border_x_coords_all = border_x_coords_all[border_x_coords_all != 0]
+
+    return(border_label_vector_all, border_mapobject_id_all, border_y_coords_all, border_x_coords_all)
+
+
+def main(args):
+
+    logger.debug('Read TM credentials')
+    tm_credentials = pd.read_csv(args.credentials_file)
+
+    logger.debug('Read metadata')
+    metadata = pd.read_csv(args.metadata_file)
+
+    if not os.path.isdir(args.output_directory):
+        logger.debug('Creating output directory {}'.format(args.output_directory))
+        os.makedirs(args.output_directory)
+
+    border_labels, border_mapobject_ids, border_y, border_x = get_borders(
+        tm_credentials = tm_credentials,
+        experiment_name = args.experiment_name,
+        metadata = metadata,
+        object_type = args.object_type,
+        mean_object_size = args.mean_object_size)
+
+    np.save(file = os.path.join(args.output_directory,"border_labels.npy"), arr=border_labels)
+    np.save(file = os.path.join(args.output_directory,"border_mapobject_ids.npy"), arr=border_mapobject_ids)
+    np.save(file = os.path.join(args.output_directory,"border_y.npy"), arr=border_y)
+    np.save(file = os.path.join(args.output_directory,"border_x.npy"), arr=border_x)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        prog='mpp',
+        description=('Generate a multiplexed pixel profile and for a set'
+                     'of objects defined in the metadata file.'
+                     )
+    )
+    parser.add_argument('metadata_file', help='path to metadata file')
+    parser.add_argument('credentials_file', help='path to tm credentials')
+    parser.add_argument('--verbose', '-v', action='count')
+    parser.add_argument('-o','--output_directory',
+                        default=os.path.join(os.getcwd(),'mpp_out'),
+                        type=str,
+                        help='output directory for results')
+    parser.add_argument('-t','--object_type', default="Nuclei", type=str,
+                        help='segmented object to derive mpp for')
+    parser.add_argument('-e','--experiment_name', default="Nuclei", type=str,
+                        help='name of the experiment on TissueMAPS')
+    parser.add_argument('-s','--mean_object_size', default=50000, type=int,
+                        help='average size of object (used to estimate memory'
+                             'storage requirements)')
+    return(parser.parse_args())
+
+
+def setup_logger(args):
+    global logger
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(
+        '%(asctime)s %(funcName)s %(levelname)s: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.setLevel(logging.INFO)
+    if args.verbose > 0:
+        logger.setLevel(logging.DEBUG)
+    return
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    setup_logger(args)
+    main(args)
+
--- mcu/pixel_profiles.py
+++ mcu/pixel_profiles.py
@@ -5,6 +5,7 @@ from tmclient import TmClient
 
 logger = logging.getLogger()
 
+
 def get_intensity_image_of_object(intensity_image,label_image,label):
     """Get a cropped numpy array of `intensity_image` using a
     segmentation provided in `label_image` for label `label`.
@@ -40,6 +41,7 @@ def get_intensity_image_of_object(intensity_image,label_image,label):
 
     mask = (segmentation_image_cropped == label)
     return(intensity_image_cropped, mask)
+
 
 def get_coordinate_images_of_object(label_image,label):
     """Get a numpy array containing the coordinates of the label image
@@ -83,6 +85,64 @@ def get_coordinate_images_of_object(label_image,label):
     mask = (segmentation_image_cropped == label)
     return(coordinate_image_y, coordinate_image_x, mask)
 
+
+def get_coordinate_images_of_border(label_image,label,thickness=3):
+    """Get a numpy array containing the coordinates of the label image
+    border for label `label`.
+
+    Parameters
+    ----------
+    label_image : ndarray
+        Segmentation image containing labelled objects `[0,n]` with `0`
+        as background.
+    label: int
+        Value corresponding to the object whose border image should be
+        returned.
+
+    Returns
+    -------
+    border_coordinate_image_y : ndarray
+        contains zero where `label_image` is not equal to `label` and
+        y coordinate (matrix row) of full `label_image` where `label_image`
+        is equal to label.
+    border_coordinate_image_x : ndarray
+        contains zero where `label_image` is not equal to `label` and
+        x coordinate (matrix column) of full `label_image` where `label_image`
+        is equal to label.
+    mask : ndarray
+        `mask` cropped to the same size, which is true for pixels
+        belonging to object with label `label`.
+    """
+    bboxes = mh.labeled.bbox(label_image)
+    border_mask = mh.labeled.borders(label_image)
+
+    segmentation_image_cropped = label_image[
+        bboxes[label,0]-thickness:bboxes[label,1]+thickness,
+        bboxes[label,2]-thickness:bboxes[label,3]+thickness]
+    border_mask_cropped = border_mask[
+        bboxes[label,0]-thickness:bboxes[label,1]+thickness,
+        bboxes[label,2]-thickness:bboxes[label,3]+thickness]
+
+    y_len = segmentation_image_cropped.shape[0]
+    x_len = segmentation_image_cropped.shape[1]
+
+    border_coordinate_image_y = np.transpose(np.tile(np.arange(bboxes[label,0]-thickness,
+                                                               bboxes[label,1]+thickness),(x_len,1)))
+    border_coordinate_image_x = np.tile(np.arange(bboxes[label,2]-thickness,
+                                                  bboxes[label,3]+thickness),(y_len,1))
+
+    assert border_coordinate_image_y.shape == border_coordinate_image_x.shape
+
+    object_mask = (segmentation_image_cropped == label)
+
+    # mask images by requested label
+    border_mask_cropped[object_mask] = 0
+    border = mh.dilate(border_mask_cropped,Bc=mh.disk(thickness))
+    border[object_mask==False] = 0
+
+    return(border_coordinate_image_y, border_coordinate_image_x, border)
+
+
 def get_intensity_vector_for_object(intensity_image,label_image,label):
     """Get a single-clannel intensity vector of `intensity_image` using a
     segmentation provided in `label_image` for label `label`.
@@ -109,6 +169,7 @@ def get_intensity_vector_for_object(intensity_image,label_image,label):
     v = v.reshape(v.shape[0]*v.shape[1])
     mask = mask.reshape(mask.shape[0]*mask.shape[1])
     return(v[mask])
+
 
 def get_coord_vectors_for_object(label_image,label):
     """Get lists of y and x coordinates corresponding to the pixels
@@ -138,6 +199,38 @@ def get_coord_vectors_for_object(label_image,label):
     x = x.reshape(x.shape[0]*x.shape[1])
     mask = mask.reshape(mask.shape[0]*mask.shape[1])
     return(y[mask],x[mask])
+
+
+def get_border_coord_vectors_for_object(label_image,label,thickness=3):
+    """Get lists of y and x coordinates corresponding to the pixels
+    contained in object with label `label`.
+
+    Parameters
+    ----------
+    label_image : ndarray
+        Segmentation image containing labelled objects `[0,n]` with `0`
+        as background.
+    label: int
+        Value corresponding to the object that should be returned.
+
+    Returns
+    -------
+    y : ndarray
+        1D vector containing y coordinates from `label_image`
+        for object with label `label` in `label_image`
+    x : ndarray
+        1D vector containing x coordinates from `label_image`
+        for object with label `label` in `label_image`
+    """
+    y, x, mask = get_coordinate_images_of_border(label_image,label,thickness)
+    assert y.shape == mask.shape
+    assert x.shape == mask.shape
+    y = y.reshape(y.shape[0]*y.shape[1])
+    x = x.reshape(x.shape[0]*x.shape[1])
+    mask = mask.reshape(mask.shape[0]*mask.shape[1])
+    label = np.repeat(label, mask.shape[0])
+    return(y[mask],x[mask],label[mask])
+
 
 def get_mpp_matrix_for_objects(tm, channel_names, object_type, labels, plate_name, well_name, well_pos_y, well_pos_x):
     """Generate multiplexed pixel profiles
@@ -220,3 +313,71 @@ def get_mpp_matrix_for_objects(tm, channel_names, object_type, labels, plate_nam
 
     # return mpp matrix
     return np.transpose(all_pixel_profiles), label_vector, y_coords, x_coords
+
+
+def get_borders_for_objects(tm, object_type, labels, plate_name, well_name, well_pos_y, well_pos_x):
+    """Generate multiplexed pixel profiles
+
+    Parameters
+    ----------
+    tm: TmClient
+        TmClient object to faciliate connections with TissueMAPS database
+    channel_names: list
+        List of channel names of type `str`. These should match the channel
+        names on the TissueMAPS instance
+    object_type : str
+        Label object name.
+    labels: list
+        List containing `int` values specifying the object labels.
+    plate_name: str
+        Plate name
+    well_name: str
+        Well name
+    well_pos_y: int
+        Y-coordinate of site in well
+    well_pos_x: int
+        X-coordinate of site in well
+
+    Returns
+    -------
+    border_label_vector : ndarray
+        1D vector with length equal to the total number of pixels in all
+        objects, containing the labels corresponding to the rows of `mpp`
+    border_y_coords : ndarray
+        1D vector with length equal to the total number of pixels in all
+        objects, containing the y-coordinates corresponding to the rows
+        of `mpp`
+    border_x_coords : ndarray
+        1D vector with length equal to the total number of pixels in all
+        objects, containing the x-coordinates corresponding to the rows
+        of `mpp`
+    """
+
+    # download segmentation
+    logger.debug('requesting download of segmentation for object {} at {}, {}, y = {}, x = {}'.format(object_type,plate_name,well_name,well_pos_y,well_pos_x))
+    segmentation = tm.download_segmentation_image(
+        mapobject_type_name = object_type,
+        plate_name = plate_name,
+        well_name = well_name, well_pos_y = well_pos_y,
+        well_pos_x = well_pos_x, align=True)
+
+    # use object areas to preallocate the results matrices
+    labels.sort()
+
+    border_label_vector = []
+    border_y_coords = []
+    border_x_coords = []
+
+    logger.debug('all_pixel_profiles shape: {}'.format(all_pixel_profiles.shape))
+
+    for label in labels:
+        logger.debug('current label: {}'.format(label))
+
+        # get border coordinates (append to list)
+        b_y_coords, b_x_coords, b_label = get_border_coord_vectors_for_object(
+            label_image = segmentation, label = label, thickness = 3)
+        border_label_vector.append(b_label)
+        border_y_coords.append(b_y_coords)
+        border_x_coords.append(b_x_coords)
+
+    return np.array(border_label_vector), np.array(border_y_coords), np.array(border_x_coords)
